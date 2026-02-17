6f209e437a8d318947dfea28e487458f19145cc0
Ty Johnson <tdj182@gmail.com>
2026-02-17 16:35:07 -0600

    basename route added

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/smite2-tool">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
